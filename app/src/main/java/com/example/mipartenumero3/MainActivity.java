7c8919fe1e98a915f966561e6e15c5273f99f443
Pedro Soria <psorialopez84@gmail.com>
2020-05-08 08:34:12 -0500

    Primer Commit Mapa Basico Paso a Paso

---
+++ app/src/main/java/com/example/mipartenumero3/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.mipartenumero3;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentActivity;
+
+import android.os.Bundle;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // Obtenemos el mapa de forma asíncrona (notificará cuando esté listo)
+        SupportMapFragment mapFragment = (SupportMapFragment)
+                getSupportFragmentManager().findFragmentById(R.id.mapa);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        GoogleMap mapa = googleMap;
+        LatLng UPV = new LatLng(-18.013766, -70.255331); //Nos ubicamos en la UNJBG
+        mapa.addMarker(new MarkerOptions().position(UPV).title("Marcador Tacna"));
+        mapa.moveCamera(CameraUpdateFactory.newLatLng(UPV));
+    }
+}
