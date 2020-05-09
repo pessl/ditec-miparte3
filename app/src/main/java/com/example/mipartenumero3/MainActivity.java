960028eaa269ba128d055d76502e41af5125099b
Pedro Soria <psorialopez84@gmail.com>
2020-05-09 00:28:57 -0500

    Añadiendo localización al mapa

--- app/src/main/java/com/example/mipartenumero3/MainActivity.java
+++ app/src/main/java/com/example/mipartenumero3/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.mipartenumero3;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -37,6 +40,17 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapa.addMarker(new MarkerOptions().position(ubicacion).title("Marcador Tacna"));
         mapa.moveCamera(CameraUpdateFactory.newLatLng(ubicacion));
         mapa.setOnMapClickListener(this);
+
+        if (ContextCompat.checkSelfPermission(this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION) ==
+                PackageManager.PERMISSION_GRANTED) {
+            mapa.setMyLocationEnabled(true);
+            mapa.getUiSettings().setZoomControlsEnabled(false);
+            mapa.getUiSettings().setCompassEnabled(true);
+        } else {
+            Button btnMiPos=(Button) findViewById(R.id.btnmiubi);
+            btnMiPos.setEnabled(false);
+        }
     }
 
     public void moveCamera(View view) {
@@ -58,5 +72,12 @@ public class MainActivity extends FragmentActivity implements OnMapReadyCallback
         mapa.addMarker(new MarkerOptions().position(puntoPulsado)
                 .icon(BitmapDescriptorFactory
                         .defaultMarker(BitmapDescriptorFactory.HUE_YELLOW)));
+    }
+
+    public void ubicacion(View view) {
+        if (mapa.getMyLocation() != null)
+            mapa.animateCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(mapa.getMyLocation().getLatitude(),
+                            mapa.getMyLocation().getLongitude()), 15));
     }
 }
